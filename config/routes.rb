05021a32ebbead2c075c3e05bee91b47d992f79d
RH <regianeh@yahoo.com>
2015-12-05 15:11:20 +0000

    Added some spacing

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root :to => redirect('ideas')
 
   get "pages/info"
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
